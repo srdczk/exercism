b20914ffa4301d09f44a2599ff32a9c1237a567a
srdczk <srdyjj@163.com>
2020-02-08 13:00:39 +0800

    init

---
+++ bash/two-fer/two_fer.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ]
+then
+    echo 'One for you, one for me.'
+else
+    echo 'One for '$1', one for me.'
+fi
+
+# The following comments should help you get started:
+# - Bash is flexible. You may use functions or write a "raw" script.
+#
+# - Complex code can be made easier to read by breaking it up
+#   into functions, however this is sometimes overkill in bash.
+#
+# - You can find links about good style and other resources
+#   for Bash in './README.md'. It came with this exercise.
+#
+#   Example:
+#   # other functions here
+#   # ...
+#   # ...
+#
+
+#   main () {
+#     # your main function code here
+#   }
+#
+#   # call main with all of the positional arguments
+#   main "$@"
+#
+# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
---
+++ x86-64-assembly/two-fer/two_fer.asm
@@ -0,0 +1,6 @@
+section .text
+global two_fer
+two_fer:
+    ; 两个 参数 - char *, char *
+    ; Provide your implementation here
+    ret
